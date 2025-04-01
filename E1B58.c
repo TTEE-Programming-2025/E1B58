ba8fec4a105774bb78dd5235270fa409673f3b45
connor0318 <u11302258@o365.ttu.edu.tw>
2025-04-02 06:58:13 +0800

    個人風格畫面完成

---
+++ E1B58.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <stdio.h>
+int main(void)
+{
+	int i,passport;
+	char ch;
+	printf("              $\n");
+	printf("            $   $\n");
+	printf("           $     $\n");
+	printf("          $       $\n");
+	printf("         $         $\n");
+	printf("        $           $ \n");
+	printf("       $             $\n");
+	printf("      $               $\n");
+	printf("     $                 $\n");
+	printf(" wellcome to E1B58's homework\n");	
+	printf("      $               $\n");	
+	printf("       $             $\n");	
+	printf("        $           $\n");	
+	printf("         $         $\n");	
+	printf("          $       $\n");	
+	printf("           $     $\n");	
+	printf("            $   $\n");	
+	printf("              $\n");	
+	system("pause"); 
+    system("CLS");  
+	
+	
+	
+	
+	
+	system("pause");
+	return 0;
+		
+}
