3e3a338d1167e7053cdf8eade11b17f30dbf22c3
connor0318 <u11302258@o365.ttu.edu.tw>
2025-04-02 08:10:41 +0800

    if else結構

--- E1B58.c
+++ E1B58.c
@@ -2,8 +2,9 @@
 #include <stdio.h>
 int main(void)
 {
-	int i,passport;
+	int password;
 	char ch;
+	//個人風格畫面 
 	printf("              $\n");
 	printf("            $   $\n");
 	printf("           $     $\n");
@@ -24,6 +25,23 @@ int main(void)
 	printf("              $\n");	
 	system("pause"); 
     system("CLS");  
+	
+	//密碼驗證
+	printf("請輸入密碼:");
+	scanf("%d",&password);
+	if(password==2024)
+	{
+	
+	
+	}
+	else
+	{
+		printf("%c",'\a');
+		printf("結束程式");
+	}
+	
+	
+	
 	
 	
 	
