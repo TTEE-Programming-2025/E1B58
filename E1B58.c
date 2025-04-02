713d0fbc96eeef6f87c58e08fa9043931aeed5e9
connor0318 <u11302258@o365.ttu.edu.tw>
2025-04-02 08:23:40 +0800

    判斷字元種類

--- E1B58.c
+++ E1B58.c
@@ -31,8 +31,31 @@ int main(void)
 	scanf("%d",&password);
 	if(password==2024)
 	{
-	
-	
+		printf("welcome\n");
+		system("pause");
+		system("CLS");
+		printf("|==============================|\n");
+		printf("|  'A'...'Z' : Uppercase       |\n");
+        printf("|  'a'...'z' : Lowercase       |\n");
+        printf("|  '0'...'9' : Digit           |\n");
+        printf("|  Others    : E1B58熊紹亨     |\n");
+	    printf("|==============================|\n");
+		fflush(stdin); // 清除輸入緩衝區，避免輸入錯誤
+        printf("請輸入一個字元：");
+        scanf(" %c", &ch); 
+        //判斷字元種類
+		 if (ch >= 'A' && ch <= 'Z') {
+        printf("Uppercase\n");
+        } else if (ch >= 'a' && ch <= 'z') {
+        printf("Lowercase\n");
+        } else if (ch >= '0' && ch <= '9') {
+        printf("Digit\n");
+        } else {
+        printf("E1B58熊紹亨\n");
+        }
+
+		
+		 
 	}
 	else
 	{
