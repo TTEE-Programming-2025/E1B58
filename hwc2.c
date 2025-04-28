c454d9d069b806928d7e867d6493ec0e706ee97f
connor0318 <u11302258@o365.ttu.edu.tw>
2025-04-29 02:25:14 +0800

    直角三角形

--- hwc2.c
+++ hwc2.c
@@ -6,7 +6,8 @@
 int main(void)
 {
 	//密碼 
-	int password,i,attempts=0;
+	int password,i,attempts=0,j,k,math;
+	char ch,chwork;
 	for(i=0;i<=21;i++)
 	{
 		printf("|@|$|?|%|\n");
@@ -34,6 +35,45 @@ int main(void)
 				printf("密碼錯誤達三次，程式即將結束");
 			}
 		}
-		return 0;
+	}
+    main:
+	printf("--------------------------\n");
+	printf("| a. 畫出直角三角形      |\n");
+	printf("| b. 顯示乘表法   	 |\n");
+	printf("| c. 結束		 |\n");
+	printf("--------------------------\n");
+	ch=getche();//偵測並回覆 
+	printf("\n");
+	if(ch=='A'||ch=='a')//3.直角三角形 
+	{
+		system("CLS");
+		printf("請輸入a~n的字元\n");
+		fflush(stdin);
+		chwork=getche();
+		while(chwork<'a'||chwork>'n')
+		{
+			printf("\n請輸入a~n的字元\n");
+			chwork=getche();
+		}
+		system("CLS");
+		int rows=chwork-'a'+1;//宣告rows變數=chwork的 ascii code -'a'+1(決定要印幾行(橫的))  
+		for (i=1;i<=rows;i++) //
+		{
+			for(k=0;k<rows-i;k++)
+			{
+				printf(" ");
+			}
+        	for (j=0;j<i;j++) 
+			{
+            printf("%c", 'a' +j);
+            }
+        printf("\n");
+    	}
+	}
+	return 0;
 }
-}
+
+
+	
+
+
