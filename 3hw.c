a09abb2a497e343d944ba262caa963dc75646324
connor0318 <u11302258@o365.ttu.edu.tw>
2025-05-13 05:44:03 +0800

    個人風格

---
+++ 3hw.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+int main(void)
+{
+	int password,attempts=0,i,j,k;
+	int seats[9][9];
+	int wrong=0;
+	//個人風格 
+	printf("$$$$$$$$$\n");
+	printf(" $$$$$$$\n");
+	printf("  E1B58\n");
+	printf("   $$$\n");
+	printf("    $\n");
+return 0;
+}
