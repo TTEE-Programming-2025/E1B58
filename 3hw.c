88b1fba0b315e9bee31846e42f243f2423af9fbe
connor0318 <u11302258@o365.ttu.edu.tw>
2025-05-13 06:06:29 +0800

    選擇b部分

--- 3hw.c
+++ 3hw.c
@@ -65,6 +65,63 @@ int main(void)
                 printf("\n");
             }
         }
+ //choose b
+ else if (choice == 'b') {
+            int need;
+            printf("需要幾個座位？(1~4)：");
+            scanf("%d", &need);
+            if (need < 1 || need > 4) {
+                printf("數量不合法。\n");
+                continue;
+            }
+     int found = 0;
+            for (i = 0; i < 9 && found == 0; i++) {
+                for (j = 0; j <= 9 - need; j++) {
+                    int ok = 1;
+                    for (k = 0; k < need; k++) {
+                        if (seats[i][j + k] != 0) {
+                            ok = 0;
+                            break;
+                        }
+                    }
+                    if (ok) {
+                        for (k = 0; k < need; k++)
+                            seats[i][j + k] = 2;
+                        found = 1;
+                        break;
+                    }
+                }
+            }
+
+            if (!found) {
+                printf("找不到連續座位。\n");
+                continue;
+            }
+
+            printf(" 123456789\n");
+            for (i = 8; i >= 0; i--) {
+                printf("%d", i + 1);
+                for (j = 0; j < 9; j++) {
+                    if (seats[i][j] == 0) printf("-");
+                    else if (seats[i][j] == 1) printf("*");
+                    else if (seats[i][j] == 2) printf("@");
+                }
+                printf("\n");
+            }
+
+            char ok;
+            printf("是否滿意這些座位？(y/n)：");
+            scanf(" %c", &ok);
+            for (i = 0; i < 9; i++) {
+                for (j = 0; j < 9; j++) {
+                    if (seats[i][j] == 2) {
+                        if (ok == 'y') seats[i][j] = 1;
+                        else seats[i][j] = 0;
+                    }
+                }
+            }
+        }
 }
+    
 return 0;
 }
